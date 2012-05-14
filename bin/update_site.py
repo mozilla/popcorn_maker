02eac722afe64dc7fdbb39140cf047a2d38913a8
Alfredo Aguirre <alfredo@madewithbyt.es>
2012-05-14 19:22:53 +0100

    Bugfix: Removed silly typo.
    Thanks Chris!

--- bin/update_site.py
+++ bin/update_site.py
@@ -52,7 +52,7 @@ def update_site(env, debug):
     # Commands to apply any migration and syncdb
     commands = [
         (CHDIR, here),
-        s(EXEC, 'python manage.py collectstatic --noinput'),
+        (EXEC, 'python manage.py collectstatic --noinput'),
         (EXEC, 'python manage.py syncdb'),
         (EXEC, 'python manage.py migrate'),
         ]
