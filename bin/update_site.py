4c0642df176e07870eee03879cf7e0b2f8ba622e
Alfredo Aguirre <alfredo@madewithbyt.es>
2012-05-16 17:15:57 +0100

    Updated site script amended

--- bin/update_site.py
+++ bin/update_site.py
@@ -52,9 +52,10 @@ def update_site(env, debug):
     # Commands to apply any migration and syncdb
     commands = [
         (CHDIR, here),
-        (EXEC, 'python manage.py collectstatic --noinput'),
         (EXEC, 'python manage.py syncdb'),
         (EXEC, 'python manage.py migrate'),
+        (EXEC, 'python manage.py collectstatic --noinput'),
+        (EXEC, 'python manage.py popcorn_import_butter'),
         ]
     for cmd, cmd_args in commands:
         if CHDIR == cmd:
