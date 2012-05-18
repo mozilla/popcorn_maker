4cf34dfec01b09c92f1d533a48f114121e79f4fe
Alfredo Aguirre <alfredo@madewithbyt.es>
2012-05-18 13:54:56 +0100

    Removed reset_db for the development server

--- bin/update_site.py
+++ bin/update_site.py
@@ -52,7 +52,6 @@ def update_site(env, debug):
     # Commands to apply any migration and syncdb
     commands = [
         (CHDIR, here),
-        (EXEC, 'python manage.py reset_db --router=default --noinput'),
         (EXEC, 'python manage.py syncdb --noinput'),
         (EXEC, 'python manage.py migrate --noinput'),
         (EXEC, 'python manage.py collectstatic --noinput'),
