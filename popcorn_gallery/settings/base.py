b25040ec7d445ec204fe5f577a966d5a8eaa3962
Alfredo Aguirre <alfredo@madewithbyt.es>
2012-06-02 09:20:59 +0100

    Added memcached to the default settings

--- popcorn_gallery/settings/base.py
+++ popcorn_gallery/settings/base.py
@@ -163,3 +163,9 @@ HAYSTACK_CONNECTIONS = {
         },
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
